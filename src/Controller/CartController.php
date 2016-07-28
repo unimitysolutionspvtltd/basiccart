2cecbffeeaee2c55cd63800a6c6f4a1bfda71382
karthikeyan-unimity <karthikeyan@unimity.com>
2016-07-28 18:55:20 +0530

    added to cart text fix

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -54,15 +54,16 @@ class CartController extends ControllerBase
   public function add_to_cart($nid) {
     \Drupal::service('page_cache_kill_switch')->trigger();
     $query = \Drupal::request()->query;
+    $utility = new Utility();
+    $config = $utility::cart_settings();
     $param['entitytype'] = $query->get('entitytype') ?  $query->get('entitytype') : "node";
     $param['quantity'] = $query->get('quantity') ? (is_numeric($query->get('quantity')) ? (int) $query->get('quantity') : 1) : 1;
-    Utility::add_to_cart($nid, $param);
+    $utility::add_to_cart($nid, $param);
     drupal_get_messages();
     $response = new \stdClass();
     $response->status = TRUE;
-    $response->text = '<p class="messages messages--status">'.t('Added to cart').'</p>';
+    $response->text = '<p class="messages messages--status">'.t($config->get('added_to_cart_message')).'</p>';
     $response->id = 'ajax-addtocart-message-'.$nid;
-    $utility = new Utility();
     $response->block = $utility->get_cart_content();
     return new JsonResponse($response);
   }
